320070d9b8c14520c0a4ddc07e7b9319e078562e
leo <leo.me0602@gmail.com>
2026-05-08 17:53:38 +0200

    feat: set default values for ConsumerConfig attributes and initialize settings

--- apps/consumer_delivery/core/config.py
+++ apps/consumer_delivery/core/config.py
@@ -9,10 +9,10 @@ class AuthType(enum.Enum):
 
 
 class ConsumerConfig(BaseModel):
-  output_format: str
-  data_frequency: int
-  endpoint_url: str
-  auth: AuthType
+  output_format: str = 'json'
+  data_frequency: int = 10
+  endpoint_url: str = 'http://localhost:8001'
+  auth: AuthType = AuthType.JWT
 
 
 class Settings(BaseSettings):
@@ -20,6 +20,35 @@ class Settings(BaseSettings):
   web_dashboard: ConsumerConfig
   researcher: ConsumerConfig
   ml: ConsumerConfig
+
+
+settings = Settings(
+  smart_watch=ConsumerConfig(
+    output_format='json',
+    data_frequency=5*1000,
+    endpoint_url= '',
+    auth=AuthType.JWT
+  ),
+  web_dashboard=ConsumerConfig(
+    output_format='json',
+    data_frequency=1000,
+    endpoint_url='',
+    auth=AuthType.JWT
+  ),
+  researcher=ConsumerConfig(
+    output_format='csv',
+    data_frequency=10*60*1000,
+    endpoint_url='',
+    auth=AuthType.JWT
+  ),
+  ml=ConsumerConfig(
+    output_format='json',
+    data_frequency=60*1000,
+    endpoint_url='',
+    auth=AuthType.API_KEY
+  )
+)
+
 
 
 
